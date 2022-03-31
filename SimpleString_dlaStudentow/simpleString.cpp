92e3ec20c8bc7425f3d5b6eb0c365e43d211295f
G.Bazior <bazior@agh.edu.pl>
2022-03-31 14:02:53 +0200

    Added exercise to implement SimpleString (easier version of std::string)

---
+++ SimpleString_dlaStudentow/simpleString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+#include "simpleString.h"
+
+#ifndef _MSC_FULL_VER // if not Visual Studio Compiler
+    #warning "Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym"
+#else
+    #pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
+#endif
